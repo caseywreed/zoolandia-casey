5d9833690d48d1d3eea6ce2e6feabafcb398a5ba
Casey Reed <casey.w.reed@gmail.com>
2016-10-06 11:46:33 -0500

    Empty property in Felis.cs

--- Animals/Felis.cs
+++ Animals/Felis.cs
@@ -5,8 +5,6 @@ namespace Zoolandia
     public class FelisCatus : Animal
     {
         public bool declawed {get; set;}
-        public  {get; set;}
-
         public static void knockShitOffCounter()
         {
             Console.WriteLine($"The cat took your favorite shotglass and knocked it off the counter");
