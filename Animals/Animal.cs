e9cc44c06e66f28a154bbdf82d6dc8b143a4e249
Casey Reed <casey.w.reed@gmail.com>
2016-10-06 11:59:44 -0500

    Attempting first build

--- Animals/Animal.cs
+++ Animals/Animal.cs
@@ -5,7 +5,7 @@ namespace Zoolandia
     public class Animal
     {
         public string name {get; set;}
-        public string weight {get; set;}
+        public int weightInLbs {get; set;}
         public static void sleep()
         {
             Console.WriteLine("Animal is asleep!");
--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Zoolandia
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal catInstance = new FelisCatus()
+            {
+                name = "Rosalita",
+                weightInLbs = 12,
+                declawed = true
+            };
+            Animal foxInstance = new VulpesVulpes()
+            {
+                name = "The Knox Fox",
+                weightInLbs = 25,
+                foxColor = "red"
+            };
+            Animal goatInstance = new CapraAegagrusHircus()
+            {
+                name = "Gilbert",
+                weightInLbs = 25,
+                numberOfHorns = 2
+            };
+            Console.WriteLine($"Your new cat, {catInstance.name}, is {catInstance.weightInLbs}LBs");
+            Console.WriteLine($"Your new fox, {foxInstance.name}, is {foxInstance.weightInLbs}LBs");
+            Console.WriteLine($"Your new goat, {goatInstance.name}, is {goatInstance.weightInLbs}LBs");
         }
     }
 }
